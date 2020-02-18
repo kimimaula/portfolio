ea1cc71d7b6c23f96e5fefdebea735f82b050daa
kimimaula <kimimaula@gmail.com>
2020-02-18 11:50:35 +0800

    cleaned up redundant dependencies

--- src/hooks/useCurrentUser.js
+++ src/hooks/useCurrentUser.js
@@ -18,8 +18,8 @@ function useCurrentUser(url) {
     const response = await axios.get(url)
     const currentuser = response.data.currentUser[0]
     setData(currentuser)
-    const promise1 = axios.get(`${process.env.REACT_APP_BASE_URL}/api/products/user/${currentuser._id}`)
-    const promise2 = axios.get(`${process.env.REACT_APP_BASE_URL}/api/orders/${currentuser.username}`)
+    const promise1 = axios.get(`/api/products/user/${currentuser._id}`)
+    const promise2 = axios.get(`/api/orders/${currentuser.username}`)
 
     try {
       const values = await Promise.all([promise1, promise2])
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import axios from 'axios'
 
 function useFetch(url) {
 
@@ -6,9 +7,8 @@ function useFetch(url) {
   const [loading, setLoading] = useState(true);
 
   async function fetchUrl() {
-    const response = await fetch(url);
-    const json = await response.json();
-    setData(json);
+    const response = await axios.get(url);
+    setData(response.data);
     setLoading(false);
   }
   useEffect(() => {
